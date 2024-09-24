bcc5e6024170e028f794bec92fed8be254132dc0
Shreyansh2saxena <shreyanshsaxena123@gmail.com>
2024-09-24 17:27:36 +0530

    j

---
+++ condition.js
@@ -0,0 +1,39 @@
+n=36
+if(n%2===0){
+    console.log('eve');
+    m='good'
+    let m2='hello'
+     const a=5 //unchangable  
+
+    
+}
+else{
+    console.log('odd');
+    
+}
+console.log(m);
+
+const w=77
+if(w%7===0 && w%11 ===0){
+    console.log(true);
+    
+} 
+
+for(let i=0;i<10;i++){
+    console.log(i);
+    
+}
+
+
+let a=10;
+while(a<10){
+    console.log(a);
+    a++ ;
+}
+
+let b=20;
+do{
+    console.log(b);
+    b++
+}
+while(b<20)
---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("hello")
+
+a = 234
+console.log(typeof a)
+
+m = "bankai"
+console.log(typeof m);
+
+[x,y] = [3, 4];
+console.log(1 === '1')
+console.log(1 == '1')
+
